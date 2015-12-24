d67e287a2d632c041d31301b37b4688ac3384cd6
Edward Hernandez <ehernandez@email.wm.edu>
2015-12-24 02:33:24 -0500

    made it prettier and more idiomatic to R

--- 2.1.R
+++ 2.1.R
@@ -1,13 +1,12 @@
 t <- 0
 f <- strsplit(readLines("input.txt"), 'x')
-c <- vector("list",(length(f)-1))
 
-for(x in 1:length(c)){
+for(x in 1:length(f)){
+  p <- rep(NA,3)
   for(n in 1:length(f[[x]])){
-    c[[x]][n] <- strtoi(sort(f[[x]])[n])
+    p[n] <- strtoi(f[[x]][n])
   }
-  c[[x]] <- l <- sort(c[[x]])
-  p <- l[1] * l[2] * 3 + l[2] * l[3] * 2 + l[3] * l[1] * 2
-  t <- t + p
+  p <- sort(p)
+  t <- t + p[1] * p[2] * 3 + p[2] * p[3] * 2 + p[3] * p[1] * 2
 }
 print(t)
