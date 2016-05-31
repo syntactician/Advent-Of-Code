93e877c836eeedbf42140f0a54548825f89f14f4
Edward Hernandez <ehernandez@email.wm.edu>
2016-05-31 03:04:32 -0400

    consolidated day 4

--- r/4.R
+++ r/4.R
@@ -3,24 +3,20 @@
 suppressMessages(require(digest))
 
 input <- 'ckczppom'
-key <- '000000'
 
 mine <- function(x, length) substr(digest(x, algo='md5', serialize=FALSE), 1, length)
 
 sift <- function(x, prefix) {
     length <- nchar(prefix)
-
     ore <- mine(x, length)
     i <- 0
 
     while (ore != prefix) {
         i <- i + 1
         ore <- mine(paste(x, i, sep = ''), length)
-
-        print(i)
-        print(ore)
     }
     i
 }
 
-coin <- sift(input, key)
+fiveCoin <- sift(input, '00000')
+sixCoin <- sift(input, '000000')
