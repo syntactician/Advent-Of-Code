50824dd644476987199d975633ac39e5006b4ef5
Edward Hernandez <ehernandez@email.wm.edu>
2016-06-27 18:04:56 -0400

    embraced ES6 arrows

--- js/6.js
+++ js/6.js
@@ -1,81 +1,72 @@
 #!/usr/bin/env node
-
 'use strict'
 
 const fs = require('fs')
 
-function parse (str) {
-  return str.split('\n')
-    .filter(x => x.length > 0)
-    .map(function (y) {
-      const parse = y.match(/(.*) (\d+),(\d+) through (\d+),(\d+)/)
-      return {
-        action: parse[1],
-        start: {
-          x: Math.min(parse[2], parse[4]),
-          y: Math.min(parse[3], parse[5])
-        },
-        end: {
-          x: Math.max(parse[2], parse[4]),
-          y: Math.max(parse[3], parse[5])
-        }
+const parse = (str) => str.split('\n')
+  .filter(x => x.length > 0)
+  .map((str) => {
+    const parts = str.match(/(.*) (\d+),(\d+) through (\d+),(\d+)/)
+    return {
+      action: parts[1],
+      start: {
+        x: Math.min(parts[2], parts[4]),
+        y: Math.min(parts[3], parts[5])
+      },
+      end: {
+        x: Math.max(parts[2], parts[4]),
+        y: Math.max(parts[3], parts[5])
       }
-    })
-}
+    }
+  })
 
-function sideA (arr) {
-  function followInstruction (yard, instruction) {
+const sideA = (arr) => arr
+  .reduce((grid, instruction) => {
     for (let x = instruction.start.x; x <= instruction.end.x; ++x) {
       for (let y = instruction.start.y; y <= instruction.end.y; ++y) {
-        if (yard[x] === undefined) yard[x] = []
-        if (yard[x][y] === undefined) yard[x][y] = false
+        if (grid[x] === undefined) grid[x] = []
+        // if (grid[x][y] === undefined) grid[x][y] = false
         switch (instruction.action) {
           case 'turn on':
-            yard[x][y] = true
+            grid[x][y] = true
             break
           case 'turn off':
-            yard[x][y] = false
+            grid[x][y] = false
             break
           case 'toggle':
-            yard[x][y] = !yard[x][y]
+            grid[x][y] = grid[x][y] ? !grid[x][y] : true
             break
         }
       }
     }
-    return yard
-  }
+    return grid
+  }, [])
+  .reduce((count, row) => count + row.filter(x => x).length, 0)
 
-  return arr.reduce(followInstruction, [])
-    .reduce((count, row) => count + row.filter(z => z).length, 0)
-}
-
-function sideB (arr) {
-  function followInstruction (yard, instruction) {
+const sideB = (arr) => arr
+  .reduce((grid, instruction) => {
     for (let x = instruction.start.x; x <= instruction.end.x; ++x) {
       for (let y = instruction.start.y; y <= instruction.end.y; ++y) {
-        if (yard[x] === undefined) yard[x] = []
-        if (yard[x][y] === undefined) yard[x][y] = 0
+        if (grid[x] === undefined) grid[x] = []
+        if (grid[x][y] === undefined) grid[x][y] = 0
         switch (instruction.action) {
           case 'turn on':
-            yard[x][y] = yard[x][y] + 1
+            grid[x][y] = grid[x][y] + 1
             break
           case 'turn off':
-            yard[x][y] = yard[x][y] > 0 ? yard[x][y] - 1 : 0
+            grid[x][y] = grid[x][y] > 0 ? grid[x][y] - 1 : 0
             break
           case 'toggle':
-            yard[x][y] = yard[x][y] + 2
+            grid[x][y] = grid[x][y] + 2
             break
         }
       }
     }
-    return yard
-  }
+    return grid
+  }, [])
+  .reduce((sum, row) => sum + row.reduce((a, b) => a + (b || 0)), 0)
 
-  return arr.reduce(followInstruction, [])
-    .reduce((count, row) => count + row.reduce((a, b) => a + (b || 0)), 0)
-}
-
-function main () {
+const main = () => {
   const contents = fs.readFileSync('../6.txt', 'utf8')
   const input = parse(contents)
   console.log(sideA(input))
