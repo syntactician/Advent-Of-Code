2d309fbdde4e41d7c61527102985976bf9a70e83
Edward Hernandez <ehernandez@email.wm.edu>
2016-06-28 17:33:57 -0400

    more reasonable function names

--- js/4.js
+++ js/4.js
@@ -6,17 +6,18 @@ const fs = require('fs')
 
 const parse = (str) => str.slice(0, -1)
 
-const shared = (input, test) => {
+const mine = (input, test) => {
   const hash = (str) => md5(str).slice(0, test.length)
+
   let i = 0
   for (; hash(input + i) != test; ++i)
     continue
   return i
 }
 
-const sideA = (str) => shared(str, '00000')
+const sideA = (str) => mine(str, '00000')
 
-const sideB = (str) => shared(str, '000000')
+const sideB = (str) => mine(str, '000000')
 
 const main = () => {
   const contents = fs.readFileSync('../4.txt', 'utf8')
