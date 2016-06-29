77cbca19348ec8a2e135987e09b4deb484016270
Edward Hernandez <ehernandez@email.wm.edu>
2016-06-29 15:44:24 -0400

    solved 10

---
+++ js/10.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+'use strict'
+
+const fs = require('fs')
+
+const rle = (str) => str
+  .replace(/(.)\1*/g, (seq, pl) => seq.length.toString() + pl)
+
+const sideA = (str) => {
+  let x = str
+  for (let i = 0; i < 40; ++i) {
+    x = rle(x)
+  }
+  return x.length
+}
+
+const sideB = (str) => {
+  let x = str
+  for (let i = 0; i < 50; ++i) {
+    x = rle(x)
+  }
+  return x.length
+}
+
+const main = () => {
+  const input = '3113322113'
+  console.log(sideA(input))
+  console.log(sideB(input))
+}
+
+main()
