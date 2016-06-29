40a524430972951647f58a53fc6892685f888a07
Edward Hernandez <ehernandez@email.wm.edu>
2016-06-29 04:47:20 -0400

    green for the ground

--- r/1V.R
+++ r/1V.R
@@ -12,21 +12,23 @@ parse <- function(str) {
 
 visualize <- function(input) {
   ggplot(data = input) +
-    geom_hline(yintercept = 0, color = '#3C8D0D') +
+    geom_hline(yintercept = 0, color = '#3C8D0D', alpha = 1/3) +
+    annotate('rect', xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = 0, 
+             fill = '#3C8D0D', alpha = 1/3) +
     geom_line(aes(x = x, y = y), color = '#C21717') +
-    theme( axis.line = element_blank(),
-           axis.text.x = element_blank(),
-           axis.text.y = element_blank(),
-           axis.ticks = element_blank(),
-           axis.title.x = element_blank(),
-           axis.title.y = element_blank(),
-           legend.position = 'none',
-           panel.background = element_blank(),
-           panel.border = element_blank(),
-           panel.grid.major = element_blank(),
-           panel.grid.minor = element_blank(),
-           plot.background = element_blank(),
-           plot.margin = unit(c(0, 0, 0, 0), 'mm'))
+    theme(axis.line = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          legend.position = 'none',
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.background = element_blank(),
+          plot.margin = unit(c(0, 0, 0, 0), 'mm'))
 }
 
 main <- function() {
