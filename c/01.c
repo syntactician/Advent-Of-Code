fb989976a5cc915ace91d4fc32f19e71505f2ed8
Edward Hernandez <ehernandez@email.wm.edu>
2016-07-23 15:29:46 -0400

    added day 1

---
+++ c/01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	FILE *fp;
+	int basement, c, count, floor;
+
+	fp = fopen("../input/01.txt", "r");
+	basement = count = floor = 0;
+
+	while ((c = fgetc(fp)) != EOF)
+	{
+		++count;
+
+		// assuming no invalid characters
+		(c == '(') ? ++floor : (c == ')') ? --floor : floor ;
+
+		if (!basement && floor == -1) 
+			basement = count;
+	}
+
+	fclose(fp);
+
+	printf("%d\n", floor);
+	printf("%d\n", basement);
+
+	return 0;
+}
