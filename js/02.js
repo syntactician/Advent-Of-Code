be87c1d85e3ac85305ab85f4920699cba513c928
Edward Hernandez <ehernandez@email.wm.edu>
2016-07-30 18:55:28 -0400

    variable naming consistency

--- js/02.js
+++ js/02.js
@@ -9,7 +9,7 @@ const parse = (str) => str
   .map((x) => x
     .split('x')
     .map(x => parseInt(x, 10))
-    .sort((y, z) => y - z))
+    .sort((x, y) => x - y))
 
 const sideA = (arr) => {
   const wrap = (d) => 3 * d[0] * d[1] + 2 * d[1] * d[2] + 2 * d[2] * d[0]
