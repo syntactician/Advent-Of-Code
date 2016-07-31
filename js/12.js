7e72a2d631546f5bab03352b4046c645718676b1
Edward Hernandez <ehernandez@email.wm.edu>
2016-07-31 15:37:59 -0400

    added underscore for Object.reduce()

--- js/12.js
+++ js/12.js
@@ -1,48 +1,34 @@
 #!/usr/bin/env node
 'use strict'
 
+const _ = require('underscore')
 const fs = require('fs')
 
-const parse = JSON.parse
-
-const sideA = (obj) => {
-  const sumNums = (x, y) => typeof y === 'object'
-    ? Array.isArray(y)
-      ? x + y.reduce(sumNums, 0)
-      : x + sideA(y)
+const sideA = (obj) => _.chain(obj)
+  .reduce((x, y) => typeof y === 'object'
+    ? x + sideA(y)
     : typeof y === 'number'
       ? x + y
       : x
+    , 0)
+  .value()
 
-  return Object.keys(obj)
-    .map((x) => obj[x])
-    .reduce(sumNums, 0)
-}
-
-const sideB = (obj) => {
-  const objToArray = (x) => Object.keys(x)
-    .map((y) => x[y])
-
-  const sumNums = (x, y) => typeof y === 'object'
+const sideB = (obj) => _.chain(obj)
+  .reduce((x, y) => typeof y === 'object'
     ? Array.isArray(y)
-      ? x + y.reduce(sumNums, 0)
-      : objToArray(y).every((z) => z !== 'red')
+      ? x + sideB(y)
+      : _.every(y, (x) => x !== 'red')
         ? x + sideB(y)
         : x
     : typeof y === 'number'
       ? x + y
       : x
-
-  const arr = objToArray(obj)
-
-  return arr.every((x) => x !== 'red')
-    ? arr.reduce(sumNums, 0)
-    : 0
-}
+    , 0)
+  .value()
 
 const main = () => {
   const contents = fs.readFileSync('../input/12.json', 'utf8')
-  const input = parse(contents)
+  const input = JSON.parse(contents)
   console.log(sideA(input))
   console.log(sideB(input))
 }
